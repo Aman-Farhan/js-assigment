c505212f1165c4de25f1ee838bd2f59fc1a02692
Aman <amanfarhan2008@gmail.com>
2025-07-31 22:25:40 +0500

    sdf

--- app.js
+++ app.js
@@ -17,14 +17,14 @@
 
 // leagel Name In JAVASCRIPT:
 
-let name = "Alice";               // starts with a letter, letters allowed 
-let totalAmount = 1000;           // camelCase, descriptive 
-let _totalCount = 5;           // can start with underscore 
-let $price = 199.99;              // can start with dollar sign 
-let user123 = "user123";          // digits allowed after first character 
-const ABC_SIZE = 1024;            // uppercase naming convention for constants (common practice) 
+// let name = "Alice";               // starts with a letter, letters allowed 
+// let totalAmount = 1000;           // camelCase, descriptive 
+// let _totalCount = 5;           // can start with underscore 
+// let $price = 199.99;              // can start with dollar sign 
+// let user123 = "user123";          // digits allowed after first character 
+// const ABC_SIZE = 1024;            // uppercase naming convention for constants (common practice) 
 
-console.log(name, totalAmount, _totalCount, $price, user123, ABC_SIZE);
+// console.log(name, totalAmount, _totalCount, $price, user123, ABC_SIZE);
 
 
 
